941eceb426a811df7bace42598f228dc3aeecb5f
Chris <gto389ho@gmail.com>
2016-10-11 10:44:55 -0700

    adding new PrefTest main now in python but less sodium

--- scripts/performance_test/prefTest.py
+++ scripts/performance_test/prefTest.py
@@ -14,24 +14,27 @@ def get_status(url):
 
     return data["status"]
 
-def index_timer(url) 
+def index_timer(url): 
     status = get_status(url)
         if status == "waiting":
-            print("Instance is in the wating state: cannot time indexing")
-        else: #might want to push to a log eventually
+            print("Instance is in the wating state: cannot time the indexing")
+        else: # might want to push to a log eventually
             print("Indexing Start Time: %s " % datetime.datetime.now())
             while status == "indexing":
                 status = get_status(url)
             print("Indexing End Time: %s " % datetime.datetime.now())
 
+def releseanator(url, accession):
+    process = subprocess.call('time python3 py_encoded_tools/ENCODE_patch_set.py --key test --accession '+accession+' --field aliases:encode --data "test:Preformance" --update', shell=True)
 
+def python_get(url):
+     process = subprocess.call('python3 get.py '+url,shell=True)
 
-def run(index_time, url, locust):
-    if index_time = True:
+def run(index_time, url, locust, accession):
+    if index_time:
         index_timer(url)
-
-    process = subprocess.call('time python3 py_encoded_tools/ENCODE_patch_set.py --key test --accession ENCFF445ANP --field aliases:encode --data "test:Preformance" --update', shell=True)
-    process = subprocess.call('python3 get.py $URL_PREF',shell=True)
+    releseanator(url, accession)
+    python_get(url)
         
 def main():
     import argparse
@@ -42,6 +45,7 @@ def main():
     parser.add_argument('-t', '--index-time', default='store_true', help="Yes or No to recored indexing time")
     parser.add_argument('--url', default='https://test.encodedcc.org')
     parser.add_argument('--locust', action='store_true', help="locust file")
+    parser.add_argument('--accession', default='ENCFF445ANP', help="accession code")
     
     args = parser.parse_args()
 
